a35fac2bd964e19a7fb42995dcfcae0756b4edc8
Charles Ayeni <charles.work20@gmail.com>
2022-07-24 17:16:02 -0400

    Started

---
+++ GlobalEconomies/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace GlobalEconomies
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("World Economic Data");
+            Console.WriteLine("===================");
+
+            int yearlow = 1990;
+            int yearhigh = 1994;
+            XMLParser parser = new ("global_economies.xml");
+            string numinp = "0";
+            string inp = "";
+            while (inp != "x")
+            {
+                menu(yearlow, yearhigh);
+                inp = Console.ReadLine().ToLower();
+                if (inp == "y")
+                {
+                    bool valid = false;
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Enter the low year");
+                            numinp = Console.ReadLine();
+                            yearlow = int.Parse(numinp);
+                            Console.WriteLine("Enter the high year");
+                            numinp = Console.ReadLine();
+                            yearhigh = int.Parse(numinp);
+                            if ((yearhigh - yearlow) > 4)
+                            {
+                                yearlow = 1990;
+                                yearhigh = 1994;
+                                Console.WriteLine("The Range cannot be greater than 5 years");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Range accepted, years updated");
+                                valid = true;
+                            }
+                        }catch(Exception e)
+                        {
+                            Console.WriteLine("Invalid input try again");
+                        }
+                    } while (!valid);
+                  
+                }
+                else if(inp == "r")
+                {
+                    parser.QueryXMLForRegionalSummary(yearlow, yearhigh, "Canada");
+                }
+                else if(inp == "m")
+                {
+                    Console.WriteLine("does nothing atm");
+                }
+                else if(inp == "x")
+                {
+                    Console.WriteLine("Bye bye");
+                    break; 
+                }  
+            }
+            
+
+
+        }
+
+        public static void menu(int yearlow, int yearhigh)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("'Y' to change the year range for the data (currently " + yearlow + " to " + yearhigh + ")");
+            Console.WriteLine("'R' to print all the metric data for a specific country");
+            Console.WriteLine("'M' to print the specific metric for all regions");
+            Console.WriteLine("'X' to exit the program");
+        }
+
+
+
+    }
+}
---
+++ GlobalEconomies/XMLParser.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GlobalEconomies
+{
+    public class XMLParser
+    {
+        private static XmlDocument? document;
+
+        public XMLParser(string fileName)
+        {
+            document = new XmlDocument();
+            document.Load(fileName);
+        }
+
+        public string GetAllRegionNames()
+        {
+            string results = "";
+            XmlNodeList nodes = document?.SelectNodes("/global_economies/region/@rname");
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                results += nodes[i].Value + "\n";
+            }
+            return results;
+        }
+        public string GetAllRegionIDs()
+        {
+            string results = "";
+            XmlNodeList nodes = document?.SelectNodes("/global_economies/region/@rid");
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                results += nodes[i].Value + "\n";
+            }
+            return results;
+        }
+        
+        public XmlNode GetRegionByName(string name)
+        {
+            XmlNodeList nodes = document?.SelectNodes("/global_economies/region[@rname='" + name + "']");
+            return nodes[0];
+        }
+
+        
+        private List<XmlNode> GetRelevantLabelsAndAttributes()
+        {
+            List<XmlNode> returns = new();
+            XmlNodeList labelNodes = document.SelectNodes(".//labels");
+            var t = labelNodes[0].ChildNodes;
+            for (int i = 3; i < t.Count; i++)
+            {
+                returns.Add(t[i]);
+            }
+            return returns;
+        }
+
+        //R option
+        public void QueryXMLForRegionalSummary(int minYear, int maxYear, string region)
+        {
+            string result = "";
+            var lbls = GetRelevantLabelsAndAttributes();
+            var t = document.SelectNodes($"/global_economies/region[@rid = \"{GetRegionByName(region).Attributes["rid"].Value}\"]");
+            XmlNodeList nodes = document.SelectNodes($"/global_economies/region[@rid = \"{GetRegionByName(region).Attributes["rid"].Value}\"]/year[@yid >= \"{minYear}\" and @yid <= \"{maxYear}\"]");
+
+            Console.WriteLine($"Economic Information for {region}" +
+                              $"\n----------------------------------\n");
+            Console.WriteLine("Ecnomic Metric\t");
+        }
+    }
+}
